b819624a663d7948b10aaf90d0a9826e107f152f
Hemanth1000 <lokahemanthreddy@gmail.com>
2021-10-18 10:45:07 -0400

    speech synthesis

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Speech.Synthesis;
+using Windows.UI.Xaml.Controls;
+using Windows.Media.SpeechSynthesis;
+using System.Speech;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+           SynthesizeAudioAsync();
+        }
+
+        public static void SynthesizeAudioAsync()
+        {
+            var config = SpeechConfig.FromSubscription("c4d8ff0e196c46d69ecd6f9e3cbca15d", "eastus");
+            config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
+
+            var synthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+            synthesizer.Speak("Now the computer is speaking to you.");
+            
+            //System.Speech.Synthesis.SpeechSynthesizer speechSynthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+            //PromptBuilder promptBuilder = new PromptBuilder();
+            //SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
+            //speechSynthesizer.Speak("Hello");
+
+        }
+
+    }
+}
